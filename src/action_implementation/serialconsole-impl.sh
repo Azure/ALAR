dd2f20459ccb61d6c9297825e2cb4ea9ffef6b91
malachma <malachma@microsoft.com>
2023-01-20 18:09:15 +0100

    simplified the serial paramter gneration

--- src/action_implementation/serialconsole-impl.sh
+++ src/action_implementation/serialconsole-impl.sh
@@ -14,8 +14,21 @@ enable_sysreq() {
     fi
 }
 
+alter_serial_properties() {
+    # The aim of this funtion is to provide access to the grub_serial console
+    # as well allow the OS to communicate with the serialconsole
+    # Just simple append operations are used in this case. Which is enough to gt access to a system
+    # if further adjusting is required the administrator needs to perform these steps later on after he/she got access to the system
 
-serial_fix_suse_redhat (){
+    echo "# Inserted by Azure Linux Autorecovery Tool" >> $grub_file
+    echo "# -----------------------------------------" >> $grub_file
+    echo "GRUB_TIMEOUT=10" >> $grub_file
+    echo 'GRUB_CMDLINE_LINUX="console=tty1 console=ttyS0 earlyprintk=ttyS0"' >> $grub_file
+    echo 'GRUB_SERIAL_COMMAND="serial --speed=9600 --unit=0 --word=8 --parity=no --stop=1"' >> $grub_file
+    echo 'GRUB_TIMEOUT_STYLE=""' >> $grub_file
+}
+
+serial_fix_suse_redhat () {
     if [[ "$isRedHat6" == "true" ]]; then
         echo "Configuring the serialconsole for RedHat 6.x is not implemented"
         exit 1
@@ -25,41 +38,11 @@ serial_fix_suse_redhat (){
     enable_sysreq
 
     if [[ -f $grub_file ]]; then
-        sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=30/' $grub_file
-
-	grep -Eq '^GRUB_CMDLINE_LINUX.*' $grub_file
-	if [[ $? -eq 0  ]]; then
-        	sed -i '/GRUB_CMDLINE_LINUX.*/s/"$//; s|GRUB_CMDLINE_LINUX.*|& console=tty1 console=ttyS0,115200n8 earlyprintk=ttyS0,115200"|' $grub_file
-	else
-		echo 'GRUB_CMDLINE_LINUX="console=tty1 console=ttyS0,115200n8 earlyprintk=ttyS0,115200"' >> $grub_file
-	fi
-
-
-        # modify terminal
-        grep -q 'GRUB_TERMINAL' $grub_file
-        if [[ $? -eq 0 ]]; then
-            sed -i 's/GRUB_TERMINAL.*/GRUB_TERMINAL="serial"/' $grub_file
-        else
-            echo 'GRUB_TERMINAL="serial"' >> $grub_file
-        fi
-
-        # modify GRUB serial
-        grep -q 'GRUB_SERIAL_COMMAND' $grub_file
-        if [[ $? -eq 0 ]]; then
-            sed -i 's/GRUB_SERIAL_COMMAND.*/GRUB_SERIAL_COMMAND="serial --speed=9600 --unit=0 --word=8 --parity=no --stop=1"/' $grub_file
-        else
-            echo 'GRUB_SERIAL_COMMAND="serial --speed=9600 --unit=0 --word=8 --parity=no --stop=1"' >> $grub_file
-        fi
-      
-      grep -q 'GRUB_TIMEOUT_STYLE' $grub_file
-      if [[ $? -eq 0 ]]; then
-           sed -i 's/GRUB_TIMEOUT_STYLE.*//' $grub_file
-      fi
+        alter_serial_properties 
     else
-        
-# file does not exist
-touch $grub_file
-cat << EOF > $grub_file
+    # file does not exist
+    touch $grub_file
+    cat << EOF > $grub_file
 GRUB_TIMEOUT=30
 GRUB_DISTRIBUTOR="$(sed 's, release .*$,,g' /etc/system-release)"
 GRUB_DEFAULT=saved
@@ -70,11 +53,16 @@ GRUB_DISABLE_RECOVERY="true"
 GRUB_SERIAL_COMMAND="serial --speed=9600 --unit=0 --word=8 --parity=no --stop=1"
 EOF
     fi
+    
     # update grub
     if [[ -d /sys/firmware/efi ]]; then 
         if [[ $isRedHat == "true" ]]; then
             grub2-mkconfig -o /boot/efi/EFI/$(grep '^ID=' /etc/os-release | cut -d '"' -f2)/grub.cfg
         fi    
+
+        if [[ $isSuse == "true" ]]; then
+            grub2-mkconfig -o /boot/grub2/grub.cfg
+        fi
     else
         grub2-mkconfig -o /boot/grub2/grub.cfg
     fi
@@ -92,38 +80,17 @@ if [[ "$isSuse" == "true" ]]; then
 fi
 
 # UBUNTU PART
-# if #1
 if [[ "$isUbuntu" == "true" ]]; then
     grub_file="/etc/default/grub.d/50-cloudimg-settings.cfg"
     enable_sysreq
-    
-    # if #2
+
     if [[ -f $grub_file ]]; then
-        sed -i 's/GRUB_TIMEOUT=.*/GRUB_TIMEOUT=10/' $grub_file
-	grep -Eq '^GRUB_CMDLINE_LINUX.*' $grub_file
-
-    # if #3
-	if [[ $? -eq 0  ]]; then
-        	sed -i '/GRUB_CMDLINE_LINUX.*/s/"$//; s|GRUB_CMDLINE_LINUX.*|& console=tty1 console=ttyS0 earlyprintk=ttyS0"|' $grub_file
-	else
-		echo 'GRUB_CMDLINE_LINUX="console=tty1 console=ttyS0 earlyprintk=ttyS0"' >> $grub_file
-	fi # close if#3
-
-        # modify GRUB serial if required
-        grep -q "GRUB_TERMINAL=serial" $grub_file
-        
-        # if#4
-        if [[ $? -ne 0 ]]; then
-            echo "GRUB_TERMINAL=serial" >>  $grub_file
-        else
-        # make a full replacement
-            sed -i 's/GRUB_SERIAL_COMMAND.*/GRUB_SERIAL_COMMAND="serial --speed=9600 --unit=0 --word=8 --parity=no --stop=1"/' $grub_file
-        fi # close if#4
+        alter_serial_properties
+        update-grub
     else
-
-# file does not exist
-touch $grub_file
-cat $grub_file << EOF
+    # file does not exist
+    touch $grub_file
+    cat << EOF > $grub_file
 # Set the default commandline
 GRUB_CMDLINE_LINUX="console=tty1 console=ttyS0 earlyprintk=ttyS0"
 GRUB_CMDLINE_LINUX_DEFAULT=""
@@ -140,7 +107,7 @@ GRUB_RECORDFAIL_TIMEOUT=30
 # Wait briefly on grub prompt
 GRUB_TIMEOUT=10
 EOF
-    fi # close if#2
     # update grub
     update-grub
-fi # close if#1
+    fi 
+fi
