f3d07bc5ca84198b52c82a6595031cb1002216f1
malachma <malachma@microsoft.com>
2023-02-20 11:01:10 +0100

    removed file from grep query

--- src/run-alar.sh
+++ src/run-alar.sh
@@ -2,7 +2,7 @@
 
 cd /tmp
 # Get version of ALAR and fetch it
-VERSION=$(curl -s -L https://raw.githubusercontent.com/Azure/ALAR/cli-test/Cargo.toml | grep -i VERSION Cargo.toml | head -n1 | cut -d'=' -f2 | sed 's/"//g')
+VERSION=$(curl -s -L https://raw.githubusercontent.com/Azure/ALAR/cli-test/Cargo.toml | grep -i VERSION | head -n1 | cut -d'=' -f2 | sed 's/"//g')
 curl -s -o alar2 -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar2
 chmod 700 alar2
 
