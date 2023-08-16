e767e7255f715510541c70b5c50db2b932c6836d
malachma <malachma@microsoft.com>
2023-08-16 18:36:41 +0200

    Adding support for aarch64

--- src/run-alar.sh
+++ src/run-alar.sh
@@ -3,9 +3,22 @@
 cd /tmp
 # Get version of ALAR and fetch it
 VERSION=$(curl -s -L https://raw.githubusercontent.com/Azure/ALAR/main/Cargo.toml | grep  -i VERSION | cut -f3 -d' ' | cut -c2-6)
-curl -s -o alar2 -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar2
-chmod 700 alar2
 
-# Start the recovery 
-./alar2 $1
-exit $?
+# What architecture we are running on?
+ARCH=$(uname -m)
+
+if [[ ${ARCH} == "aarch64" ]]; then
+        curl -s -o alar2 -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar2-aarch64
+        chmod 700 alar2
+
+        # Start the recovery
+        ./alar2 $1
+        exit $?
+else
+        curl -s -o alar2 -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar2
+        chmod 700 alar2
+
+        # Start the recovery
+        ./alar2 $1
+        exit $?
+fi
