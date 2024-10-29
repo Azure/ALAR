6e587ec2cd49ee8708ee1dac3b4b0eca853bb6e9
malachma <malachma@microsoft.com>
2024-10-29 19:21:31 +0100

    modified for integration tests withvm repair run

--- run-alar.sh
+++ run-alar.sh
@@ -2,23 +2,26 @@
 
 cd /tmp
 # Get version of ALAR and fetch it
-VERSION=$(curl -s -L https://raw.githubusercontent.com/Azure/ALAR/main/Cargo.toml | grep  -i VERSION | cut -f3 -d' ' | cut -c2-6)
+VERSION=$(curl -s -L https://raw.githubusercontent.com/Azure/ALAR/ALAR-redesign/Cargo.toml | grep  -i VERSION | cut -f3 -d' ' | cut -c2-6)
+#VERSION=$(curl -s -L https://raw.githubusercontent.com/Azure/ALAR/main/Cargo.toml | grep  -i VERSION | cut -f3 -d' ' | cut -c2-6)
 
 # What architecture we are running on?
 ARCH=$(uname -m)
 
 if [[ ${ARCH} == "aarch64" ]]; then
-        curl -s -o alar -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar-aarch64
+        #curl -s -o alar -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar-aarch64
+        curl -s -o alar -L https://github.com/Azure/ALAR/releases/download/test-0.9.0/alar-aarch64
         chmod 700 alar
 
         # Start the recovery
-        ./alar2 $1
+        RUST_LOG=info ./alar $@
         exit $?
 else
-        curl -s -o alar -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar
+        #curl -s -o alar -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar
+        curl -s -o alar -L https://github.com/Azure/ALAR/releases/download/test-0.9.0/alar
         chmod 700 alar
 
         # Start the recovery
-        ./alar $@
+        RUST_LOG=info ./alar $@
         exit $?
 fi
