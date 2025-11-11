5953b06acdde2d30efc251e7e66cd9ea2317c63f
malachma <malachma@microsoft.com>
2025-11-11 12:03:26 +0100

    Incorret merge, added missing functionality

--- src/run-alar.sh
+++ src/run-alar.sh
@@ -17,13 +17,13 @@ if [[ ${ARCH} == "aarch64" ]]; then
         chmod 700 alar
 
         # Start the recovery
-        RUST_LOG=info ./alar $@
+        RUST_LOG=info ./alar "${args[@]}"
         exit $?
 else
         curl -s -o alar -L https://github.com/Azure/ALAR/releases/download/v$VERSION/alar
         chmod 700 alar
 
         # Start the recovery
-        RUST_LOG=info ./alar $@
+        RUST_LOG=info ./alar "${args[@]}"
         exit $?
 fi
